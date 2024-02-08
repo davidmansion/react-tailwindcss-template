9b4af16520cd46f310ce7f06c7650732d4b15491
davidmansion <contact@davidmansion.com>
2024-02-08 08:10:53 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1
+        className="text-3xl text-center text-red-700"
+      >Welcome to Vite with TailwindCSS and React</h1>
+    </>
+  )
+}
+
+export default App
